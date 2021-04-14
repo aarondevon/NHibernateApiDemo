c94b4d2184d9d28977ddf629b5498549bc4c73d0
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-04-14 08:46:56 -0500

    Add project files.

---
+++ API/Controllers/StudentController.cs
@@ -0,0 +1,64 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private ISessionFactory _factory;
+
+        public StudentController(ISessionFactory factory)
+        {
+            _factory = factory;
+        }
+
+        // GET: api/<StudentController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Student>> Get()
+        {
+            using (var session = _factory.OpenSession())
+            {
+                var query = session.Query<Student>();
+                return query.ToList();
+            }
+        }
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET api/<StudentController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<StudentController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<StudentController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<StudentController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ API/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Student
+    {
+        public virtual int ID { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string FirstMidName { get; set; }
+    }
+}
